d707e107b9ca2ca06cdff709fdce359e789a119b
groupguanfang <1203970284@qq.com>
2025-07-23 03:54:42 +0800

    feat: update docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,21 +6,14 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Arkcode',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
+					label: '创建项目',
+					autogenerate: { directory: 'create-project' }
+				}
+			]
 		}),
 	],
 });
