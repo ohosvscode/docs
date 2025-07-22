09e92c5501e5325fc66fab2e2b726588e7f69e46
groupguanfang <1203970284@qq.com>
2025-07-23 04:47:59 +0800

    chore: add eslint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,19 @@
+import starlight from '@astrojs/starlight'
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Arkcode',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: '创建项目',
-					autogenerate: { directory: 'create-project' }
-				}
-			]
-		}),
-	],
-});
+  integrations: [
+    starlight({
+      title: 'Arkcode',
+      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+      sidebar: [
+        {
+          label: '创建项目',
+          autogenerate: { directory: 'create-project' },
+        },
+      ],
+    }),
+  ],
+})
