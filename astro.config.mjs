bf39657f22a43963627167b73b27a2585487290c
groupguanfang <1203970284@qq.com>
2025-07-23 04:55:04 +0800

    feat: update

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,13 +7,8 @@ export default defineConfig({
   integrations: [
     starlight({
       title: 'Arkcode',
-      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-      sidebar: [
-        {
-          label: '创建项目',
-          autogenerate: { directory: 'create-project' },
-        },
-      ],
+      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/ohosvscode' }],
+      sidebar: [],
     }),
   ],
 })
