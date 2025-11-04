596479044ed064e2baca0d3b2887e6f4e4c41b4a
groupguanfang <1203970284@qq.com>
2025-11-05 01:24:06 +0800

    feat: update docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,25 @@
 import netlify from '@astrojs/netlify'
 import starlight from '@astrojs/starlight'
 import { defineConfig } from 'astro/config'
+import unocss from 'unocss/astro'
 
 // https://astro.build/config
 export default defineConfig({
   adapter: netlify(),
   integrations: [
+    unocss(),
     starlight({
       title: 'Arkcode',
       social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/ohosvscode' }],
       sidebar: [
         { label: 'Naily\'s ArkTS Support', autogenerate: { directory: 'arkts' } },
       ],
+      locales: {
+        root: {
+          label: '简体中文',
+          lang: 'zh-CN',
+        },
+      },
     }),
   ],
 })
